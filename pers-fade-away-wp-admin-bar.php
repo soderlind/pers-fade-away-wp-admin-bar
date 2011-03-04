2b82c3c99f777bf9a3dfec9d5d6dbd1bb4b87f39
soderlind <per@soderlind.no>
2011-03-04 16:25:58 +0000

    initial release
    git-svn-id: http://plugins.svn.wordpress.org/pers-fade-away-wp-admin-bar/trunk@355268 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ pers-fade-away-wp-admin-bar.php
@@ -0,0 +1,76 @@
+<?php
+/*
+Plugin Name: PerS Fade Away WordPress Admin Bar
+Plugin URI: http://soderlind.no/archives/2011/02/25/fade-away-wordpress-admin-bar/
+Description: Fade away the WordPress Admin Bar when you scroll down the page.
+Version: 1.0.1
+Author: PerS
+Author URI: http://soderlind.no
+*/
+
+/*
+Change log:
+	1.0.1 - Thanks to @TheFrosty, added is_admin_bar_showing()
+    1.0.0 - initial release
+*/
+
+if (!class_exists('ps_pers_fade_wp_admin_bar')) {
+    class ps_pers_fade_wp_admin_bar {
+		var $url = '';
+		function __construct(){
+			$this->url = plugins_url(basename(__FILE__), __FILE__);
+			add_action('wp_print_scripts', array(&$this,'ps_pers_fade_wp_admin_bar_script'));
+		}
+		function ps_pers_fade_wp_admin_bar_script() {
+				if (is_admin_bar_showing()) {
+					wp_enqueue_script('jQuery('); // other scripts included with Wordpress: http://tinyurl.com/y875age
+					wp_enqueue_script('ps_pers_fade_wp_admin_bar_script', $this->url.'?ps_pers_fade_wp_admin_bar_javascript'); // embed javascript, see end of this file
+				}
+			}
+	} //End Class
+} //End if class exists statement
+
+
+
+if (isset($_GET['ps_pers_fade_wp_admin_bar_javascript'])) {
+	//embed javascript
+	Header("content-type: application/x-javascript");
+	echo<<<ENDJS
+/**
+* @desc PerS Fade Away WP Admin Bar
+* @author PerS - http://soderlind.no
+*/
+// Script from http://tympanus.net/codrops/2009/12/11/fixed-fade-out-menu-a-css-and-jquery-tutorial/
+jQuery(document).ready(function(){
+		jQuery(window).scroll(function(){
+			var scrollTop = jQuery(window).scrollTop();
+			if(scrollTop != 0)
+				jQuery('#wpadminbar').stop().animate({'opacity':'0.2'},400);
+			else
+				jQuery('#wpadminbar').stop().animate({'opacity':'1'},400);
+		});
+
+		jQuery('#wpadminbar').hover(
+			function (e) {
+				var scrollTop = jQuery(window).scrollTop();
+				if(scrollTop != 0){
+					jQuery('#wpadminbar').stop().animate({'opacity':'1'},400);
+				}
+			},
+			function (e) {
+				var scrollTop = jQuery(window).scrollTop();
+				if(scrollTop != 0){
+					jQuery('#wpadminbar').stop().animate({'opacity':'0.2'},400);
+				}
+			}
+		);
+});
+
+ENDJS;
+
+} else {
+	if (class_exists('ps_pers_fade_wp_admin_bar')) { 
+    	$ps_pers_fade_wp_admin_bar_var = new ps_pers_fade_wp_admin_bar();
+	}
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== PerS Fade Away WordPress Admin Bar ===
+Contributors: PerS
+Donate link: http://soderlind.no/donate/
+Tags: admin bar, hide, fade
+Requires at least: 3.1
+Tested up to: 3.1
+Stable tag: trunk
+
+Fade away the WordPress Admin Bar when you scroll down the page.
+
+== Description ==
+
+Instead of disabling the WordPress 3.1 Admin Bar, use this plugin, based on [Codrops Fixed Fade Out Menu](http://tympanus.net/Tutorials/FixedFadeOutMenu/), to fade away the WordPress Admin Bar when you scroll down the page.
+
+For more information, please see the [plugin home page](http://soderlind.no/archives/2011/02/25/fade-away-wordpress-admin-bar/) 
+
+== Installation ==
+
+= Manual Installation =
+* Upload the files to wp-content/plugins/pers-fade-away-wp-admin-bar/
+* Activate the plugin
+
+= Automatic Installation =
+* On your WordPress blog, open the Dashboard
+* Go to Plugins->Install New
+* Search for "fade away admin bar"
+* Click on install to install PerS Fade Away WordPress Admin Bart
+
+
+== Changelog ==
+
+= 1.0.1 = 
+* Thanks to @TheFrosty, added is_admin_bar_showing()
+= 1.0.0 = 
+* initial release
